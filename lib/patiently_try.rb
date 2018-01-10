ff6cc6f0e3aae33d0c028b3e4928099caec8a5a3
Fabian Zitter <Fabian@Fabians-iMac-2.local>
2018-01-10 12:50:37 +0900

    Reduce complexity

--- lib/patiently_try.rb
+++ lib/patiently_try.rb
@@ -2,34 +2,48 @@ require "patiently_try/version"
 
 module PatientlyTry
   def patiently_try(opts = {})
-    retries         = opts[:retries] || 100
-    wait            = opts[:wait] || 0
-    catch_errors    = Array(opts[:catch] || StandardError)
-    excluded_errors = Array(opts[:raise_if_caught])
-    logging         = opts[:logging].nil? ? true : opts[:logging]
-    try             = 0
+    opts = _parse_opts(opts)
+    try  = 0
 
     begin
       yield
-    rescue *(catch_errors) => e
+    rescue *(opts[:catch]) => e
       try += 1
 
-      _log_error(e) if logging
+      _log_error(e) if opts[:logging]
 
-      if try >= retries || _rescued_but_excluded?(e, excluded_errors)
-        _log_backtrace(e) if logging
+      if _exceeded_retries?(try, opts[:retries]) || _rescued_but_excluded?(e, opts[:raise_if_caught])
+        _log_backtrace(e) if opts[:logging]
         raise e
       end
 
-      _log_retry(e) if logging
+      _log_retry(e) if opts[:logging]
 
-      sleep wait if wait && wait > 0
+      _wait(opts[:wait])
 
       retry
     end
   end
 
   private
+
+  def _parse_opts(opts)
+    {
+      catch:           Array(opts[:catch] || StandardError),
+      raise_if_caught: Array(opts[:raise_if_caught]),
+      wait:            opts[:wait].to_i,
+      logging:         opts[:logging].nil? ? true : opts[:logging],
+      retries:         opts[:retries] || 100
+    }
+  end
+
+  def _wait(wait_time)
+    sleep wait_time if wait_time > 0
+  end
+
+  def _exceeded_retries?(try, retries)
+    try >= retries
+  end
 
   def _log_retry
     puts "Retrying (#{try}/#{retries})"
